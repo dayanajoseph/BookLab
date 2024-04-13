3d5bc1d8218c4cdf19f3c2f3fbd40eb64b20c828
Dayana Joseph <dayanajosephofficial@gmail.com>
2024-04-13 10:52:25 -0400

    passchange

--- database.go
+++ database.go
@@ -8,7 +8,7 @@ import (
 )
 
 func Connect() (*sql.DB, error) {
-	dsn := "root:Djm@1211@tcp(localhost:3306)/booksDB"
+	dsn := "root:Bella1211!@tcp(localhost:3306)/booksDB"
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		log.Fatal(err)
